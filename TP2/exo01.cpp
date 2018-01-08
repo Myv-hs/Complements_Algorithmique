2ab96faa2baced3e3bbb02a1992881625f46838b
Myv-hs <matthewcoyle@me.com>
2018-01-08 23:17:49 +0100

    TP2exo01 fini

--- TP2/exo01.cpp
+++ TP2/exo01.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void p (int n) {
+	if(n==0)return;
+	cout << "Bonjour\n";
+	return p(n-1);
+}
+
+int f (int n) {
+	if(n==1) return 1;
+	return n*n+f(n-1); 
+}
+
 int main () {
-	
+	int x; 
+	cout << "Entrez n : ";cin>>x;
+	p(x);
+	cout << "La somme des "<<x<<" premiers carres est : "<<f(x)<<endl;
+
 	return 0;
 }
